86d8288b9752017af75aedd698b67d63bfbb2e75
Gabriel Veliz <gvelizzuniga@gmail.com>
2024-05-19 22:51:20 -0400

    se agregan link para fronteras

--- src/components/CountryPage.jsx
+++ src/components/CountryPage.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
+import "../styles/CountryPage.css";
 
 const CountryPage = () => {
     const { countryName } = useParams();
@@ -36,33 +38,39 @@ const CountryPage = () => {
     return <div><p>Loading Country...</p></div>;
     }
 
-    const currencies = pais.currencies ? Object.values(pais.currencies).map(c => c.name).join(', ') : 'N/A';
-    const nativeName = pais.name.nativeName ? Object.values(pais.name.nativeName)[0].official : 'N/A';
-    const languages = pais.languages ? Object.values(pais.languages).join(', ') : 'N/A';
+    const currencies = pais.currencies ? Object.values(pais.currencies).map(c => c.name).join(', ') : 'No currencies';
+    const nativeName = pais.name.nativeName ? Object.values(pais.name.nativeName)[0].official : 'No Native Name';
+    const languages = pais.languages ? Object.values(pais.languages).join(', ') : 'No Languages';
 
     return (
-    <div>
+    <div className='CountryPage'>
         <div>
             <img src={pais.flags.png} alt={pais.flags.alt} />
         </div>
         <div>
-            <div><p>{countryName}</p></div>
-        <div>
+            <div className='CountryName'><p>{countryName}</p></div>
+        <div className='CountryDescription'>
             <div>
-            <p>Native Name: {nativeName}</p>
-            <p>Population: {pais.population}</p>
-            <p>Region: {pais.region}</p>
-            <p>Sub Region: {pais.subregion}</p>
-            <p>Capital: {pais.capital && pais.capital.join(', ')}</p>
+                <p>Native Name: {nativeName}</p>
+                <p>Population: {pais.population}</p>
+                <p>Region: {pais.region}</p>
+                <p>Sub Region: {pais.subregion}</p>
+                <p>Capital: {pais.capital && pais.capital.join(', ')}</p>
             </div>
             <div>
-            <p>Top Level Domain: {pais.tld}</p>
-            <p>Currencies: {currencies}</p>
-            <p>Languages: {languages}</p>
+                <p>Top Level Domain: {pais.tld}</p>
+                <p>Currencies: {currencies}</p>
+                <p>Languages: {languages}</p>
             </div>
         </div>
-        <div>
-            <p>Border Countries: {borderCountries.length > 0 ? borderCountries.join(', ') : 'There are no borders'}</p>
+        <div className='BordersGrid'>
+            <p>Border Countries: </p>{borderCountries.length > 0 ? 
+            (
+                borderCountries.map((borderCountry, index) => (
+                    <Link key={index} to={`/country/${borderCountry}`}><div className='BorderBox' ><p>{borderCountry}</p></div></Link>
+                ))
+                ) 
+            : 'No borders'}
         </div>
         </div>
     </div>
